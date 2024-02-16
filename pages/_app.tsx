057e35178c2a6eaa7989a5356b899e73e52900d9
Jonathan Case <jpcase104@gmail.com>
2024-02-16 16:23:21 -0500

    Add github auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Algolia Assessment JC</title> 
+      </Head>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />;
+      </SessionProvider>
+    </>
+  );
 }
